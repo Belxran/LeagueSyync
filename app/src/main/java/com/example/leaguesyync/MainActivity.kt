9351053b584e6ae8b5819af24002345d964fa2bb
Belxran <133035564+Belxrann@users.noreply.github.com>
2024-03-06 17:58:14 +0100

    Prova

--- app/src/main/java/com/example/leaguesyync/MainActivity.kt
+++ app/src/main/java/com/example/leaguesyync/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.leaguesyync
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        registerButton.setOnClickListener {
+            // Acción al hacer clic en el botón de registro
+            // Aquí puedes abrir la actividad de registro o cualquier otra acción que desees
+        }
+
+        loginButton.setOnClickListener {
+            // Acción al hacer clic en el botón de inicio de sesión
+            // Aquí puedes abrir la actividad de inicio de sesión o cualquier otra acción que desees
+        }
     }
 }
