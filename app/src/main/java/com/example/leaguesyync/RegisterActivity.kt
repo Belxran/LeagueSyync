da56df4a23153f39cd55e8299af0481136bc60d8
Belxran <133035564+Belxrann@users.noreply.github.com>
2024-03-08 17:29:47 +0100

    modificacion xml

--- app/src/main/java/com/example/leaguesyync/RegisterActivity.kt
+++ app/src/main/java/com/example/leaguesyync/RegisterActivity.kt
@@ -2,21 +2,36 @@ package com.example.leaguesyync
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import java.io.File
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val registerButton = findViewById<Button>(R.id.registerButton)
+        val btnSave = findViewById<Button>(R.id.registerButton)
+        val etName = findViewById<EditText>(R.id.firstNameEditText)
+        val etLastName1 = findViewById<EditText>(R.id.lastName1EditText)
+        val etLastName2 = findViewById<EditText>(R.id.lastName2EditText)
+        val etUsername = findViewById<EditText>(R.id.usernameEditText)
+        val etPassword = findViewById<EditText>(R.id.confirmPasswordEditText)
 
-        registerButton.setOnClickListener {
-            // Aquí deberías implementar la lógica para registrar al usuario
-            // Puedes realizar la validación de los datos ingresados por el usuario
-            // y guardar la información en tu base de datos o en otro sistema de almacenamiento
-            // Una vez registrado, puedes abrir la actividad de inicio de sesión o cualquier otra acción que desees
-            finish()
+        btnSave.setOnClickListener {
+            val name = etName.text.toString()
+            val lastName1 = etLastName1.text.toString()
+            val lastName2 = etLastName2.text.toString()
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            // Crear un objeto Usuario con los datos ingresados
+
+            // Guardar el usuario en el archivo JSON
+
+            // Aquí puedes agregar más lógica, como mostrar un mensaje de éxito o redirigir a otra actividad
         }
     }
 }
---
+++ app/src/main/java/com/example/leaguesyync/Usuario.kt
@@ -0,0 +1,13 @@
+package com.example.leaguesyync
+import org.json.JSONObject
+
+data class Usuario(
+    val nombre: String,
+    val apellido1: String,
+    val apellido2: String?,
+    val nombreUsuario: String,
+    val contraseña: String
+) {
+
+}
+
