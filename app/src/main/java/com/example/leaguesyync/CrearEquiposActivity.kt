9dcf2d43a9cff1830f0939f833c15e8bedaa0ec4
Belxran <133035564+Belxrann@users.noreply.github.com>
2024-03-13 19:12:58 +0100

    he cambiado la pantalla de LoginActivity

---
+++ app/src/main/java/com/example/leaguesyync/CrearEquiposActivity.kt
@@ -0,0 +1,5 @@
+package com.example.leaguesyync
+
+class CrearEquiposActivity {
+
+}
--- app/src/main/java/com/example/leaguesyync/CrearLiga.kt
+++ app/src/main/java/com/example/leaguesyync/CrearLiga.kt
@@ -1,12 +1,25 @@
 package com.example.leaguesyync
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.leaguesyync.R
+import com.example.leaguesyync.databinding.CrearLigaBinding
+
 
 class CrearLiga : AppCompatActivity() {
+    private lateinit var binding: CrearLigaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.crear_liga)
+        binding = CrearLigaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Aquí establece el evento de clic del botón para continuar
+        binding.buttonCreateLeague.setOnClickListener {
+            val numEquipos = binding.spinnerNumTeams.selectedItem.toString().toInt()
+            val intent = Intent(this, CrearEquiposActivity::class.java)
+            intent.putExtra("NUMERO_EQUIPOS", numEquipos)
+            startActivity(intent)
+        }
     }
 }
