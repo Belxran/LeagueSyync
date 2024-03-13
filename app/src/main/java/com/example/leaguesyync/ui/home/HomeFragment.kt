ea48d15ead59896358872813f5d0eed1163156d1
Belxran <133035564+Belxrann@users.noreply.github.com>
2024-03-13 20:25:03 +0100

    he cambiado la pantalla de LoginActivity

--- app/src/main/java/com/example/leaguesyync/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/leaguesyync/ui/home/HomeFragment.kt
@@ -1,6 +1,6 @@
 package com.example.leaguesyync
-import android.content.Intent
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.leaguesyync.databinding.FragmentHomeBinding
-
 
 class HomeFragment : Fragment() {
 
@@ -23,7 +22,13 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // Agregar OnClickListener al botón "buscarliga"
+        binding.buscarliga.setOnClickListener {
+            // Acción a realizar al hacer clic en el CardView
+            startActivity(Intent(activity, BuscarLigaActivity::class.java))
+        }
 
+        // Agregar OnClickListener al botón "createleague"
         binding.createleague.setOnClickListener {
             Log.d("HomeFragment", "CardView clicked")
             startActivity(Intent(activity, CrearLiga::class.java))
