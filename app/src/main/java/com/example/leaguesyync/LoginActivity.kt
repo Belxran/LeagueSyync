4dffe34a38a1adf77a09c69dc087e10e49e42ca7
Uneees <belabbes1021@gmail.com>
2024-03-19 20:13:34 +0100

    update LoginActivity.kt

--- app/src/main/java/com/example/leaguesyync/LoginActivity.kt
+++ app/src/main/java/com/example/leaguesyync/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.example.leaguesyync
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import com.google.gson.Gson
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var usuarios: List<Usuario>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -15,10 +21,34 @@ class LoginActivity : AppCompatActivity() {
         val googleButton = findViewById<ImageView>(R.id.google_btn)
         val appleButton = findViewById<ImageView>(R.id.apple_btn)
 
+        // Cargar usuarios desde el archivo JSON
+        cargarUsuariosDesdeJSON("usuarios.json")
 
         loginButton.setOnClickListener {
-            val intent = Intent(this, MenuPrincipal::class.java)
-            startActivity(intent)
+            // Datos de inicio de sesión de prueba
+            val username = "usuario1"
+            val contraseña = "contraseña1"
+
+            // Verificación del inicio de sesión
+            if (iniciarSesion(username, contraseña)) {
+                println("Inicio de sesión exitoso para el usuario: $username")
+            } else {
+                println("Error: Nombre de usuario o contraseña incorrectos")
+            }
         }
+
+    }
+
+    private fun cargarUsuariosDesdeJSON(jsonPath: String) {
+        val gson = Gson()
+        val inputStream = assets.open(jsonPath)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        usuarios = gson.fromJson(reader, Array<Usuario>::class.java).toList()
+    }
+
+    private fun iniciarSesion(username: String, contraseña: String): Boolean {
+        val usuario = usuarios.find { it.username == username }
+        return usuario?.contraseña == contraseña
     }
 }
+
