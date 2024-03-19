de40c3f539cbd06d6913e2663768b6023ae1ff4a
Belxran <133035564+Belxrann@users.noreply.github.com>
2024-03-19 18:03:02 +0100

    se tienen que solucionar fallos

--- app/src/main/java/com/example/leaguesyync/crear/CrearEquiposActivity.kt
+++ app/src/main/java/com/example/leaguesyync/crear/CrearEquiposActivity.kt
@@ -1,4 +1,4 @@
-package com.example.leaguesyync.crear
+package com.example.leaguesyync
 
 
 import android.os.Bundle
--- app/src/main/java/com/example/leaguesyync/crear/CrearLiga.kt
+++ app/src/main/java/com/example/leaguesyync/crear/CrearLiga.kt
@@ -1,4 +1,4 @@
-package com.example.leaguesyync.crear
+package com.example.leaguesyync
 
 import android.content.Intent
 import android.os.Bundle
